e0399850b47be72ff89176a32cca1e8241fba83d
Victoria Wika <victoriawika@icloud.com>
2021-03-27 13:01:45 +0100

    wip

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express()
 setupMongo()
 
 app.use('/', express.json())
-app.use(express.static('.client/build'))
+app.use(express.static('./client/build'))
 app.use('/api/sights', require('./routes/sights'))
 app.use(require('./routes/error'))
 
