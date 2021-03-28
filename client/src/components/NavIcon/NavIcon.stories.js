6ba80b685843c9c86c2fda7b3e759ced22cd56c9
Victoria Wika <victoriawika@icloud.com>
2021-03-28 23:55:28 +0200

    Fix storybook for NavIcon

--- client/src/components/NavIcon/NavIcon.stories.js
+++ client/src/components/NavIcon/NavIcon.stories.js
@@ -1,8 +1,16 @@
 import NavIcon from './NavIcon'
+import { action } from '@storybook/addon-actions'
 
 export default {
   title: 'Components',
   component: NavIcon,
 }
 
-export const PrimaryNavIcon = () => <NavIcon />
+const DefaultNavIcon = args => <NavIcon {...args} />
+
+export const PrimaryNavIcon = DefaultNavIcon.bind({})
+
+PrimaryNavIcon.args = {
+  setOpen: action('onClick'),
+  open: 'open',
+}
